c27c78f80af4623818dc181e49c1b39b1fd1d29b
Jeff Stieler <jeff.m.stieler@gmail.com>
2015-02-06 14:56:24 -0500

    Replace row and column shortcodes with divs for TinyMCE preview.

--- tinymce-plugin.js
+++ tinymce-plugin.js
@@ -5,7 +5,13 @@
 		// replace shortcodes with markup for preview
 		editor.on( 'BeforeSetContent', function( e ) {
 
+			if ( e.content ) {
 
+				e.content = wp.shortcode.replace( 'bs_col', e.content, gridShortcodeToPreview );
+
+				e.content = wp.shortcode.replace( 'bs_row', e.content, gridShortcodeToPreview );
+
+			}
 
 		});
 
@@ -15,6 +21,25 @@
 
 
 		});
+
+		/**
+		 * Convert [bs_col] and [bs_row] to preview divs
+		 *
+		 * @param wp.shortcode shortcode
+		 * @returns string preview HTML
+		 */
+		function gridShortcodeToPreview( shortcode ) {
+
+			return wp.html.string({
+				"tag": "div",
+				"attrs": {
+					"class": shortcode.get("class"),
+					"data-bs_grid_preview": 1
+				},
+				"content": shortcode.content.trim()
+			});
+
+		}
 
 	} );
 
