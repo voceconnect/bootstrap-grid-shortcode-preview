253b38b1abf7dfe115ea115bcdd00a3fd230116d
Jeff Stieler <jeff.m.stieler@gmail.com>
2015-02-06 14:21:41 -0500

    Initial commit - basic plugin that loads TinyMCE plugin stub.

---
+++ bs-grid-shortcode-ui.php
@@ -0,0 +1,38 @@
+<?php
+/*
+  Plugin Name: Bootstrap Grid Shortcode UI
+  Version: 0.1
+  Plugin URI: http://voceconnect.com/
+  Description: Renders the Bootstrap Grid shortcode in TinyMCE
+  Author: Voce Platforms, Jeff Stieler
+  Author URI: http://voceconnect.com/
+ */
+
+class Bootstrap_Grid_Shortcode_UI_Plugin {
+
+	const PLUGIN_SLUG = 'bs_grid_shortcode_ui';
+
+	function add_tinymce_plugin( $mce_external_plugins ) {
+
+		$mce_external_plugins[self::PLUGIN_SLUG] = plugins_url( 'tinymce-plugin.js', __FILE__ );
+
+		return $mce_external_plugins;
+
+	}
+
+	function attach_hooks() {
+
+		// TODO: only add the plugin if the Bootstrap Grid is enabled
+		add_filter( 'mce_external_plugins', array( $this, 'add_tinymce_plugin' ) );
+
+	}
+
+	function init() {
+
+		$this->attach_hooks();
+
+	}
+
+}
+
+add_action( 'init', array( new Bootstrap_Grid_Shortcode_UI_Plugin(), 'init' ) );
---
+++ tinymce-plugin.js
@@ -0,0 +1,21 @@
+(function() {
+
+	tinymce.PluginManager.add( 'bs_grid_shortcode_ui', function( editor, url ) {
+
+		// replace shortcodes with markup for preview
+		editor.on( 'BeforeSetContent', function( e ) {
+
+
+
+		});
+
+		// replace markup with shortcodes for saving
+		editor.on( 'PostProcess', function( e ) {
+
+
+
+		});
+
+	} );
+
+})();
